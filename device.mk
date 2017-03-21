29a50004fa956fe1fb736262f4d4692a79cbb995
Craig Donner <cdonner@google.com>
2017-03-20 17:41:48 -0700

    Add VR feature flags and HAL.
    Bug: 36097960
    Bug: 36462741
    Test: adb shell lshal on device, successfully ran Daydream Home, though I'm
    seeing some crashes in various things.
    Change-Id: Id1af18701e2021ab8c3de7889ec71c316fe4420c

--- device.mk
+++ device.mk
@@ -153,6 +153,8 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.nfc.xml:system/etc/permissions/android.hardware.nfc.xml \
     frameworks/native/data/etc/android.hardware.nfc.hce.xml:system/etc/permissions/android.hardware.nfc.hce.xml \
     frameworks/native/data/etc/android.hardware.nfc.hcef.xml:system/etc/permissions/android.hardware.nfc.hcef.xml \
+    frameworks/native/data/etc/android.hardware.vr.headtracking-0.xml:system/etc/permissions/android.hardware.vr.headtracking.xml \
+    frameworks/native/data/etc/android.hardware.vr.high_performance.xml:system/etc/permissions/android.hardware.vr.high_performance.xml \
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/init.power.sh:system/bin/init.power.sh \
@@ -318,6 +320,10 @@ PRODUCT_PACKAGES += \
     liblocation_api \
     android.hardware.gnss@1.0-impl-qti \
     android.hardware.gnss@1.0-service-qti
+
+# VR HAL
+PRODUCT_PACKAGES += \
+    android.hardware.vr@1.0-service.wahoo \
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/sec_config:system/etc/sec_config
