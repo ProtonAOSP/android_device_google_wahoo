4fd68ab1ff5a0671afcc6047635c2b5586e4ed87
Ruchi Kandoi <kandoiruchi@google.com>
2017-03-24 14:32:29 -0700

    Remove libnfc-nxp.conf
    The config files are device specific in terms of the
    clock the NFC controller uses.
    
    Test: NFC is enabled on both the devices
    Bug: 36163226
    Change-Id: I805e4581216d673741777a1aea5baa16d5c96f99
    Signed-off-by: Ruchi Kandoi<kandoiruchi@google.com>

--- device.mk
+++ device.mk
@@ -246,8 +246,7 @@ PRODUCT_PACKAGES += \
     android.hardware.nfc@1.0-impl
 
 PRODUCT_COPY_FILES += \
-    device/google/wahoo/nfc/libnfc-brcm.conf:system/etc/libnfc-brcm.conf \
-    device/google/wahoo/nfc/libnfc-nxp.conf:system/etc/libnfc-nxp.conf \
+    device/google/wahoo/nfc/libnfc-brcm.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-brcm.conf \
 
 PRODUCT_PACKAGES += \
     libmm-omxcore \
