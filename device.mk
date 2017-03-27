689da96d323370ba63438c56d020659a1110f1fb
Mikhail Naganov <mnaganov@google.com>
2017-03-27 09:47:09 -0700

    Bring up binderized Audio and Soundtrigger services on new Pixels
    Verified: audio in Hangouts, Play Music + EQ, YouTube,
        audio over A2DP and USB.
    
    Soundtrigger seems to be working--the device wakes up to lockscreen,
    need to dismiss it manually, and then Assistant prompt appears
    immediately by itself. Looks to me more like an issue with Assistant
    rather than with s/t.
    
    Bug: 35808308
    Bug: 36098105
    Test: manual
    Change-Id: I03c0e04a8f35b7da0bbef9dd32e606a07067ba36

--- device.mk
+++ device.mk
@@ -357,7 +357,8 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-impl \
     android.hardware.audio.effect@2.0-impl \
     android.hardware.broadcastradio@1.0-impl \
-    android.hardware.soundtrigger@2.0-impl
+    android.hardware.soundtrigger@2.0-impl \
+    android.hardware.audio@2.0-service
 
 # Thermal packages
 PRODUCT_PACKAGES += \
