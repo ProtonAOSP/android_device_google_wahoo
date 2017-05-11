fd33ab5147cf12a467c8dcdd6b3d3b4ea38e878d
Ray Essick <essick@google.com>
2017-05-11 16:44:39 -0700

    add preliminary codec performance characteristics
    CtsVideoTestCases were failing on video encode/decode performance
    tests for the wahoo-family devices because there is no no performance
    characteristic file in the build.  This adds an initial set of
    performance characteristics, taken verbatim from the marlin devices.
    
    The numbers will need updating before the device is released.
    
    Bug: 38134584
    Test: successful run of affected CTS tests
    Change-Id: Idd37095a2883ee1e00a2f774da23ce3202c07b04

--- device.mk
+++ device.mk
@@ -436,6 +436,7 @@ PRODUCT_COPY_FILES += \
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/media_codecs.xml:system/etc/media_codecs.xml \
+    $(LOCAL_PATH)/media_codecs_performance.xml:system/etc/media_codecs_performance.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml \
