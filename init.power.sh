747e7cad5611f4f9a5a3c17792584d8a863ed51f
Chris Fries <cfries@google.com>
2017-05-23 11:26:57 -0500

    init.power.sh: enable memlat and add SELinux context
    Settings taken from QC:
    
    From 42e89348d007cec86bf13db0996f563599dc70f7 Mon Sep 17 00:00:00 2001
    From: Amir Vajid <avajid@codeaurora.org>
    Date: Tue, 16 Aug 2016 20:00:52 -0700
    Subject: init.qcom.post_boot: enable memlat on msmcobalt
    
    Enable memlat on msmcobalt with initial settings
    to improve power and performance.
    
    Bug: 38511241
    Change-Id: I2c74d1d7e0bcb3bec55ba5af389bf1ead23d639f

--- init.power.sh
+++ init.power.sh
@@ -34,6 +34,14 @@ done
 
 write /sys/class/devfreq/soc:qcom,mincpubw/governor "cpufreq"
 
+# Enable memlat governor
+for memlat in /sys/class/devfreq/*qcom,memlat-cpu*
+do
+    write $memlat/governor "mem_latency"
+    write $memlat/polling_interval 10
+    write $memlat/mem_latency/ratio_ceil 400
+done
+
 # Signal perfd that boot has completed
 setprop sys.post_boot.parsed 1
 
