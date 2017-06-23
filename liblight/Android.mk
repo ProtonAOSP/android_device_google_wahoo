2d00ff1586d43ea5264c45ed3e635a2216ebf842
Tri Vo <trong@google.com>
2017-06-22 17:21:55 -0700

    Fix missing headers from libhardware_headers.
    Bug: 37342627
    Test: cd device/google/wahoo/ && mma -j64 -k BOARD_VNDK_VERSION=current
    Change-Id: I20c21bb9e18049d4f90d8a8102be5935b98aa2ea

--- liblight/Android.mk
+++ liblight/Android.mk
@@ -19,6 +19,7 @@ include $(CLEAR_VARS)
 LOCAL_SRC_FILES := lights.c
 LOCAL_MODULE_RELATIVE_PATH := hw
 LOCAL_PROPRIETARY_MODULE := true
+LOCAL_HEADER_LIBRARIES := libhardware_headers
 LOCAL_SHARED_LIBRARIES := libc libcutils liblog
 LOCAL_CFLAGS := $(common_flags) \
     -DLOG_TAG=\"qdlights\" \
