f594698be64d2f676f66c031733abefbdf2b85ba
Patrick Tjin <pattjin@google.com>
2017-07-17 14:19:18 -0700

    Enable console suspend if UART is not enabled
    Test: walleye with console enabled -> console_suspend = N
    Change-Id: Iea28573dd953408b78787ea17841a80fbdbe9657

--- init.power.sh
+++ init.power.sh
@@ -45,3 +45,9 @@ done
 # Signal perfd that boot has completed
 setprop sys.post_boot.parsed 1
 
+# On debuggable builds, enable console_suspend if uart is enabled to save power
+# Otherwise, disable console_suspend to get better logging for kernel crashes
+if [[ $(getprop ro.debuggable) == "1" && ! -e /sys/class/tty/ttyMSM0 ]]
+then
+    write /sys/module/printk/parameters/console_suspend N
+fi
