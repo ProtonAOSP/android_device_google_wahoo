a1f13eeb5ccac0182ae52f5783c372e7c3286a64
rago <rago@google.com>
2017-09-14 20:13:01 -0700

    Fixed virtualizer boost
    Virtualizer was adding too much energy to the effects if enabled.
    
    Bug: 65529284
    Test: manual testing with SoloTester, play music and headphone load.
    Change-Id: Ifea0995380a28239a0cec185c63425aac74a39c5
    (cherry picked from commit 1c1439d67bea95d68a0e427a7a20aa794eab149c)

--- device.mk
+++ device.mk
@@ -605,3 +605,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Preopt SystemUI
 PRODUCT_DEXPREOPT_SPEED_APPS += \
     SystemUIGoogle
+
+# audio effects config
+PRODUCT_PROPERTY_OVERRIDES += \
+    fmas.hdph_sgain=0
