6749bd83aef653ec782520ef971e9dfdcc13f720
Nick Desaulniers <ndesaulniers@google.com>
2017-10-23 11:14:37 -0700

    wahoo: fingerprint: change HAL binary name
    Change the binary name to not be device specific. This allows us not to
    have multiple init.rc files per device, simplifying code sharing between
    devices that use the same HAL.
    
    Bug: 68144019
    Change-Id: Ib81fa673c96a25137ad3dfb673f161243cc55ef4

--- device.mk
+++ device.mk
@@ -498,7 +498,7 @@ PRODUCT_COPY_FILES += \
 
 # Fingerprint HIDL implementation
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.1-service.wahoo
+    android.hardware.biometrics.fingerprint@2.1-service.fpc
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
