40b61a3bb8f08ab042f0777984b6cf7c38a722d0
Adam Seaton <aseaton@google.com>
2017-11-28 12:53:11 -0800

    Update SVN to 5 for Jan 2017 Monthly Update Bug:69257226

--- device.mk
+++ device.mk
@@ -23,7 +23,7 @@ PRODUCT_COPY_FILES += \
 
 # Set the SVN for the targeted MR release
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.vendor.build.svn=4
+    ro.vendor.build.svn=5
 
 # Enforce privapp-permissions whitelist
 PRODUCT_PROPERTY_OVERRIDES += \
