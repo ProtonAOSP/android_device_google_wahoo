a4a5db5772301ed57f6ebd6d8f64527742690a01
Ruchi Kandoi <kandoiruchi@google.com>
2018-03-01 13:59:26 -0800

    Remove system and vendor image dependency
    libnfc-nci.conf shouldn't be a part of vendor image, since it is
    accessed by libnfc-nci and Nfc application, both are part of system
    image.
    
    Test: Nfc enable/disable
    Bug: 72080121
    Change-Id: I686ba234c7d2aa923070c3fa980c56b73b18e574
    (cherry picked from commit 3197b56882ed9997043d87e6582f6ebaa0fdbace)

--- device.mk
+++ device.mk
@@ -352,7 +352,7 @@ PRODUCT_PACKAGES += \
     android.hardware.nfc@1.0-service
 
 PRODUCT_COPY_FILES += \
-    device/google/wahoo/nfc/libnfc-nci.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nci.conf \
+    device/google/wahoo/nfc/libnfc-nci.conf:$(TARGET_COPY_OUT_SYSTEM)/etc/libnfc-nci.conf \
 
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.1-service.wahoo
