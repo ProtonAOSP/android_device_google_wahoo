cf65303db1186236d02036313027cd5a3a200482
Mehdi Alizadeh <mett@google.com>
2018-03-16 14:12:52 -0700

    Removes wifi offload HAL service from Wahoo
    Removes wifi offload HAL service from Wahoo devices by removing related
    entries from wahoo/device.mk and wahoo/manifest.xml
    
    Bug: 74549432
    Test: Check logs and verify offload HAL service is not running
    Change-Id: I45799d07e49c03bac73037867fd0b1db95da495a

--- device.mk
+++ device.mk
@@ -442,7 +442,6 @@ PRODUCT_PACKAGES += $(WPA)
 # Wifi
 PRODUCT_PACKAGES += \
     android.hardware.wifi@1.0-service \
-    android.hardware.wifi.offload@1.0-service \
     wificond \
     wifilogd \
     libwpa_client
