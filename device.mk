6df16ac39c34c6bda93dc1d16dd61836f80486e6
Adam Seaton <aseaton@google.com>
2018-05-16 16:16:49 -0700

    "Update SVN to 14 for July RoW Walleye/Taimen Build Bug: 79883349"

--- device.mk
+++ device.mk
@@ -23,7 +23,7 @@ PRODUCT_COPY_FILES += \
 
 # Set the SVN for the targeted MR release
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.vendor.build.svn=13
+    ro.vendor.build.svn=14
 
 # Enforce privapp-permissions whitelist
 PRODUCT_PROPERTY_OVERRIDES += \
