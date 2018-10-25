a560dd9ade857bbfc14339b66db1a90b30201f30
emilchung <emilchung@google.com>
2018-10-25 09:29:46 +0800

    wahoo: Stop retry fingerprint init proecess after limited times
    Add new mechanism for stopping FP HIDL after several times init fail.
    This prevents the case of FP HIDL keeping trying to do init process and
    raising CPU usage when sensor totally dead.
    After limited times init retry fail (default 10 times), FP HIDL will
    be stopped.
    
    init-fingerprint-sh is for stopping fps_hal after init retry fail
    serval times.
    It keeps running until max init retry or fps_hal running successfully.
    vendor.fps_hal restarts until max init retry times fail.
    
    Test: build pass and FP functionaily are good.
    Test: init process will be stopped after limited times retry fail
    Bug: 111710758
    Change-Id: I8a19bdd351e5d712f8e23c6a0849acb1771e61e5
    Signed-off-by: emilchung <emilchung@google.com>

--- device.mk
+++ device.mk
@@ -562,6 +562,8 @@ PRODUCT_COPY_FILES += \
 # Fingerprint HIDL implementation
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.1-service.fpc
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/init.fingerprint.sh:$(TARGET_COPY_OUT_VENDOR)/bin/init.fingerprint.sh \
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
