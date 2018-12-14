455b4669442d8e4550aaf5554fe89195eea88329
Rajeev Kumar <rajekumar@google.com>
2018-12-13 16:57:23 -0800

    Make sure to not override TARGET_SYSTEM_PROP.
    Overriding TARGET_SYSTEM_PROP in wahoo/device.mk file removes system properties set for Android Go devices.
    
    Bug: 120873371
    Test: Manual tests
    Change-Id: Ib001e5df504e1ba5e146cd242019225aa203f385

--- device.mk
+++ device.mk
@@ -59,7 +59,7 @@ SRC_MEDIA_HAL_DIR := hardware/qcom/media/msm8998
 SRC_DISPLAY_HAL_DIR := hardware/qcom/display/msm8998
 SRC_CAMERA_HAL_DIR := hardware/qcom/camera/msm8998
 
-TARGET_SYSTEM_PROP := $(LOCAL_PATH)/system.prop
+TARGET_SYSTEM_PROP := $(TARGET_SYSTEM_PROP) $(LOCAL_PATH)/system.prop
 
 # Get kernel-headers
 $(call inherit-product, hardware/qcom/msm8998/msm8998.mk)
