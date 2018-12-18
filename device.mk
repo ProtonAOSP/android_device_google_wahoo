6d949a6221dbefbd079a6c693bf2a5ef11fb1126
mike dooley <mdooley@google.com>
2018-12-18 20:29:02 +0100

    Enabling sound trigger V2_2 for wahoo
    V2_2 has the same functionality as V2_1 except it adds one new
    api, getModelState.
    
    Note: V2_1 will be removed in a subsequent cl.
    
    Test: built android and tested using SoundTriggerTestApp
    
    Bug: 70206501
    Change-Id: Id2647687732e79df252588d69f95d2a407e0092c

--- device.mk
+++ device.mk
@@ -501,7 +501,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.audio@5.0-impl:32 \
     android.hardware.audio.effect@5.0-impl:32 \
-    android.hardware.soundtrigger@2.1-impl:32 \
+    android.hardware.soundtrigger@2.2-impl:32 \
     android.hardware.audio@2.0-service
 
 # stereo speakers: orientation changes swap L/R channels
