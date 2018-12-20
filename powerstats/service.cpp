61d2125575fe200064fc695609e5d11c702a20d0
Benjamin Schwartz <bsschwar@google.com>
2018-12-20 11:16:20 -0800

    power.stats: Port power.stats HAL to Pixel 2
    Bug: 120301393
    Test: run vts -m VtsHalPowerStatsV1_0Target
    Test: adb shell "lshal debug android.hardware.power.stats@1.0::IPowerStats/default"
    Change-Id: Ib46e205651e0972dd4008976209aaf7dbff4d5f8

---
+++ powerstats/service.cpp
@@ -0,0 +1,135 @@
+/*
+ * Copyright (C) 2018 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#define LOG_TAG "android.hardware.power.stats@1.0-service.pixel"
+
+#include <android/log.h>
+#include <hidl/HidlTransportSupport.h>
+
+#include <pixelpowerstats/GenericStateResidencyDataProvider.h>
+#include <pixelpowerstats/PowerStats.h>
+#include <pixelpowerstats/WlanStateResidencyDataProvider.h>
+#include "EaselStateResidencyDataProvider.h"
+
+using android::OK;
+using android::sp;
+using android::status_t;
+
+// libhwbinder:
+using android::hardware::configureRpcThreadpool;
+using android::hardware::joinRpcThreadpool;
+
+// Generated HIDL files
+using android::hardware::power::stats::V1_0::IPowerStats;
+using android::hardware::power::stats::V1_0::PowerEntityInfo;
+using android::hardware::power::stats::V1_0::PowerEntityStateSpace;
+using android::hardware::power::stats::V1_0::PowerEntityType;
+using android::hardware::power::stats::V1_0::implementation::PowerStats;
+
+// Pixel specific
+using android::hardware::google::pixel::powerstats::GenericStateResidencyDataProvider;
+using android::hardware::google::pixel::powerstats::PowerEntityConfig;
+using android::hardware::google::pixel::powerstats::StateResidencyConfig;
+using android::hardware::google::pixel::powerstats::WlanStateResidencyDataProvider;
+
+// Wahoo specific
+using android::device::google::wahoo::powerstats::EaselStateResidencyDataProvider;
+
+int main(int /* argc */, char ** /* argv */) {
+    ALOGI("power.stats service 1.0 is starting.");
+
+    PowerStats *service = new PowerStats();
+
+    // Add power entities related to rpmh
+    const uint64_t RPM_CLK = 19200;  // RPM runs at 19.2Mhz. Divide by 19200 for msec
+    std::function<uint64_t(uint64_t)> rpmConvertToMs = [](uint64_t a) { return a / RPM_CLK; };
+    std::vector<StateResidencyConfig> rpmStateResidencyConfigs = {
+        {.name = "XO_shutdown",
+         .entryCountSupported = true,
+         .entryCountPrefix = "XO Count:",
+         .totalTimeSupported = true,
+         .totalTimePrefix = "Accumulated XO duration:",
+         .totalTimeTransform = rpmConvertToMs,
+         .lastEntrySupported = false}};
+
+    auto rpmSdp =
+        std::make_shared<GenericStateResidencyDataProvider>("/d/system_stats");
+
+    uint32_t apssId = service->addPowerEntity("APSS", PowerEntityType::SUBSYSTEM);
+    rpmSdp->addEntity(apssId, PowerEntityConfig("APSS", rpmStateResidencyConfigs));
+
+    uint32_t mpssId = service->addPowerEntity("MPSS", PowerEntityType::SUBSYSTEM);
+    rpmSdp->addEntity(mpssId, PowerEntityConfig("MPSS", rpmStateResidencyConfigs));
+
+    uint32_t adspId = service->addPowerEntity("ADSP", PowerEntityType::SUBSYSTEM);
+    rpmSdp->addEntity(adspId, PowerEntityConfig("ADSP", rpmStateResidencyConfigs));
+
+    uint32_t slpiId = service->addPowerEntity("SLPI", PowerEntityType::SUBSYSTEM);
+    rpmSdp->addEntity(slpiId, PowerEntityConfig("SLPI", rpmStateResidencyConfigs));
+
+    service->addStateResidencyDataProvider(std::move(rpmSdp));
+
+    // Add SoC power entity
+    std::vector<StateResidencyConfig> socStateResidencyConfigs = {
+        {.name = "XO_shutdown",
+         .header = "RPM Mode:vlow",
+         .entryCountSupported = true,
+         .entryCountPrefix = "count:",
+         .totalTimeSupported = true,
+         .totalTimePrefix = "actual last sleep(msec):",
+         .lastEntrySupported = false},
+        {.name = "VMIN",
+         .header = "RPM Mode:vmin",
+         .entryCountSupported = true,
+         .entryCountPrefix = "count:",
+         .totalTimeSupported = true,
+         .totalTimePrefix = "actual last sleep(msec):",
+         .lastEntrySupported = false}};
+
+    auto socSdp =
+        std::make_shared<GenericStateResidencyDataProvider>("/d/system_stats");
+
+    uint32_t socId = service->addPowerEntity("SoC", PowerEntityType::POWER_DOMAIN);
+    socSdp->addEntity(socId, PowerEntityConfig(socStateResidencyConfigs));
+
+    service->addStateResidencyDataProvider(std::move(socSdp));
+
+    // Add WLAN power entity
+    uint32_t wlanId = service->addPowerEntity("WLAN", PowerEntityType::SUBSYSTEM);
+    auto wlanSdp = std::make_shared<WlanStateResidencyDataProvider>(wlanId);
+    service->addStateResidencyDataProvider(std::move(wlanSdp));
+
+    // Add Easel power entity
+    uint32_t easelId = service->addPowerEntity("Easel", PowerEntityType::SUBSYSTEM);
+    auto easelSdp = std::make_shared<EaselStateResidencyDataProvider>(easelId);
+    service->addStateResidencyDataProvider(std::move(easelSdp));
+
+    // Configure the threadpool
+    configureRpcThreadpool(1, true /*callerWillJoin*/);
+
+    status_t status = service->registerAsService();
+    if (status != OK) {
+        ALOGE("Could not register service for power.stats HAL Iface (%d), exiting.", status);
+        return 1;
+    }
+
+    ALOGI("power.stats service is ready");
+    joinRpcThreadpool();
+
+    // In normal operation, we don't expect the thread pool to exit
+    ALOGE("power.stats service is shutting down");
+    return 1;
+}
