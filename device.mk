6cb71af67bfeee32c81bba9530df8f4468fd8bd3
Jiyong Park <jiyong@google.com>
2019-03-15 03:24:45 +0000

    Revert^2 "Set ro.apex.updatable to true for wahoo"
    This reverts commit 0000d909aa6e24b873b3fd0a081366b8f7800904.
    
    Reason for revert: ag/6729196 will fix the previous problem
    
    Bug: 129035564
    Test: wahoo is bootable, sdcard works
    Change-Id: Ifa15cf6d6b0480428c3f9de823829bae6475f2ee

--- device.mk
+++ device.mk
@@ -732,3 +732,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
     debug.sf.early_gl_phase_offset_ns=3000000
 PRODUCT_PROPERTY_OVERRIDES += \
     debug.sf.early_gl_app_phase_offset_ns=15000000
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.apex.updatable=true
