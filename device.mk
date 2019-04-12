7a2c0c55abad0e8e31b48211d4b78749ca615408
Paul Scovanner <pscovanner@google.com>
2019-04-12 12:00:25 -0700

    [DO NOT MERGE] Update Wahoo SVN to 27 Bug:129374896 (cherry picked from commit 9424dc7bc50693498f0e418dceb49abac3af03f2)
    Change-Id: Iabe1d0e575afb7c3f2ac4818e1198c580d8d333a

--- device.mk
+++ device.mk
@@ -36,7 +36,7 @@ PRODUCT_COPY_FILES += \
 
 # Set the SVN for the targeted MR release
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.vendor.build.svn=26
+    ro.vendor.build.svn=27
 
 # Enforce privapp-permissions whitelist
 PRODUCT_PROPERTY_OVERRIDES += \
