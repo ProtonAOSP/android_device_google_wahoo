0234c54d3087dff447451ea1164631416347a0ef
Harpreet "Eli" Sangha <eliptus@google.com>
2019-05-13 18:14:07 +0900

    vibrator: Add Unit Tests
    Bug: 130261570
    Test: PTS, VTS, Manual
    Change-Id: I7f1d906fbf0fc4ad36ce29ac71627ac3b9053069
    Signed-off-by: Harpreet "Eli" Sangha <eliptus@google.com>

---
+++ vibrator/tests/mocks.h
@@ -0,0 +1,57 @@
+/*
+ * Copyright (C) 2019 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#ifndef ANDROID_HARDWARE_VIBRATOR_TEST_MOCKS_H
+#define ANDROID_HARDWARE_VIBRATOR_TEST_MOCKS_H
+
+#include "Vibrator.h"
+
+class MockApi : public ::android::hardware::vibrator::V1_2::implementation::Vibrator::HwApi {
+  public:
+    MOCK_METHOD0(destructor, void());
+    MOCK_METHOD1(setAutocal, bool(std::string value));
+    MOCK_METHOD1(setOlLraPeriod, bool(uint32_t value));
+    MOCK_METHOD1(setActivate, bool(bool value));
+    MOCK_METHOD1(setDuration, bool(uint32_t value));
+    MOCK_METHOD1(setState, bool(bool value));
+    MOCK_METHOD0(hasRtpInput, bool());
+    MOCK_METHOD1(setRtpInput, bool(int8_t value));
+    MOCK_METHOD1(setMode, bool(std::string value));
+    MOCK_METHOD1(setSequencer, bool(std::string value));
+    MOCK_METHOD1(setScale, bool(uint8_t value));
+    MOCK_METHOD1(setCtrlLoop, bool(bool value));
+    MOCK_METHOD1(setLpTriggerEffect, bool(uint32_t value));
+    MOCK_METHOD1(debug, void(int fd));
+
+    ~MockApi() override { destructor(); };
+};
+
+class MockCal : public ::android::hardware::vibrator::V1_2::implementation::Vibrator::HwCal {
+  public:
+    MOCK_METHOD0(destructor, void());
+    MOCK_METHOD1(getAutocal, bool(std::string &value));
+    MOCK_METHOD1(getLraPeriod, bool(uint32_t *value));
+    MOCK_METHOD1(getClickDuration, bool(uint32_t *value));
+    MOCK_METHOD1(getTickDuration, bool(uint32_t *value));
+    MOCK_METHOD1(getDoubleClickDuration, bool(uint32_t *value));
+    MOCK_METHOD1(getHeavyClickDuration, bool(uint32_t *value));
+    MOCK_METHOD1(debug, void(int fd));
+
+    ~MockCal() override { destructor(); };
+    // b/132668253: Workaround gMock Compilation Issue
+    bool getAutocal(std::string *value) { return getAutocal(*value); }
+};
+
+#endif  // ANDROID_HARDWARE_VIBRATOR_TEST_MOCKS_H
---
+++ vibrator/tests/test-hwcal.cpp
@@ -0,0 +1,247 @@
+/*
+ * Copyright (C) 2019 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#define LOG_TAG "PtsVibratorHalWahooTestSuite"
+
+#include <android-base/file.h>
+#include <android-base/properties.h>
+#include <gtest/gtest.h>
+
+#include <fstream>
+
+#include "Hardware.h"
+
+using namespace ::testing;
+
+namespace android {
+namespace hardware {
+namespace vibrator {
+namespace V1_2 {
+namespace implementation {
+
+using ::android::base::SetProperty;
+using ::android::base::WaitForProperty;
+
+class HwCalTest : public Test {
+  protected:
+    static constexpr char PROPERTY_PREFIX[] = "test.vibrator.hal.";
+
+    static constexpr uint32_t DEFAULT_CLICK_DURATION_MS = 6;
+    static constexpr uint32_t DEFAULT_TICK_DURATION_MS = 2;
+    static constexpr uint32_t DEFAULT_DOUBLE_CLICK_DURATION_MS = 135;
+    static constexpr uint32_t DEFAULT_HEAVY_CLICK_DURATION_MS = 8;
+
+  public:
+    void SetUp() override {
+        setenv("PROPERTY_PREFIX", PROPERTY_PREFIX, true);
+        setenv("CALIBRATION_FILEPATH", mCalFile.path, true);
+    }
+
+  private:
+    template <typename T>
+    static void pack(std::ostream &stream, const T &value, std::string lpad, std::string rpad) {
+        stream << lpad << value << rpad;
+    }
+
+  protected:
+    void createHwCal() { mHwCal = std::make_unique<HwCal>(); }
+
+    template <typename T>
+    void write(const std::string key, const T &value, std::string lpad = " ",
+               std::string rpad = "") {
+        std::ofstream calfile{mCalFile.path, std::ios_base::app};
+        calfile << key << ":";
+        pack(calfile, value, lpad, rpad);
+        calfile << std::endl;
+    }
+
+    void unlink() { ::unlink(mCalFile.path); }
+
+  protected:
+    std::unique_ptr<Vibrator::HwCal> mHwCal;
+    TemporaryFile mCalFile;
+};
+
+TEST_F(HwCalTest, click_present) {
+    std::string prefix{PROPERTY_PREFIX};
+    uint32_t expect = std::rand();
+    uint32_t actual = ~expect;
+
+    EXPECT_TRUE(SetProperty(prefix + "click.duration", std::to_string(expect)));
+
+    createHwCal();
+
+    EXPECT_TRUE(mHwCal->getClickDuration(&actual));
+    EXPECT_EQ(expect, actual);
+}
+
+TEST_F(HwCalTest, click_missing) {
+    std::string prefix{PROPERTY_PREFIX};
+    uint32_t expect = DEFAULT_CLICK_DURATION_MS;
+    uint32_t actual = ~expect;
+
+    EXPECT_TRUE(SetProperty(prefix + "click.duration", std::string()));
+
+    createHwCal();
+
+    EXPECT_TRUE(mHwCal->getClickDuration(&actual));
+    EXPECT_EQ(expect, actual);
+}
+
+TEST_F(HwCalTest, tick_present) {
+    std::string prefix{PROPERTY_PREFIX};
+    uint32_t expect = std::rand();
+    uint32_t actual = ~expect;
+
+    EXPECT_TRUE(SetProperty(prefix + "tick.duration", std::to_string(expect)));
+
+    createHwCal();
+
+    EXPECT_TRUE(mHwCal->getTickDuration(&actual));
+    EXPECT_EQ(expect, actual);
+}
+
+TEST_F(HwCalTest, tick_missing) {
+    std::string prefix{PROPERTY_PREFIX};
+    uint32_t expect = DEFAULT_TICK_DURATION_MS;
+    uint32_t actual = ~expect;
+
+    EXPECT_TRUE(SetProperty(prefix + "tick.duration", std::string()));
+
+    createHwCal();
+
+    EXPECT_TRUE(mHwCal->getTickDuration(&actual));
+    EXPECT_EQ(expect, actual);
+}
+
+TEST_F(HwCalTest, doubleclick) {
+    std::string prefix{PROPERTY_PREFIX};
+    uint32_t expect = DEFAULT_DOUBLE_CLICK_DURATION_MS;
+    uint32_t actual = ~expect;
+
+    createHwCal();
+
+    EXPECT_TRUE(mHwCal->getDoubleClickDuration(&actual));
+    EXPECT_EQ(expect, actual);
+}
+
+TEST_F(HwCalTest, heavyclick_present) {
+    std::string prefix{PROPERTY_PREFIX};
+    uint32_t expect = std::rand();
+    uint32_t actual = ~expect;
+
+    EXPECT_TRUE(SetProperty(prefix + "heavyclick.duration", std::to_string(expect)));
+
+    createHwCal();
+
+    EXPECT_TRUE(mHwCal->getHeavyClickDuration(&actual));
+    EXPECT_EQ(expect, actual);
+}
+
+TEST_F(HwCalTest, heavyclick_missing) {
+    std::string prefix{PROPERTY_PREFIX};
+    uint32_t expect = DEFAULT_HEAVY_CLICK_DURATION_MS;
+    uint32_t actual = ~expect;
+
+    EXPECT_TRUE(SetProperty(prefix + "heavyclick.duration", std::string()));
+
+    createHwCal();
+
+    EXPECT_TRUE(mHwCal->getHeavyClickDuration(&actual));
+    EXPECT_EQ(expect, actual);
+}
+
+TEST_F(HwCalTest, autocal_present) {
+    std::string expect = std::to_string(std::rand()) + " " + std::to_string(std::rand()) + " " +
+                         std::to_string(std::rand());
+    std::string actual = "";
+
+    write("autocal", expect);
+
+    createHwCal();
+
+    EXPECT_TRUE(mHwCal->getAutocal(&actual));
+    EXPECT_EQ(expect, actual);
+}
+
+TEST_F(HwCalTest, autocal_missing) {
+    std::string actual;
+
+    createHwCal();
+
+    EXPECT_FALSE(mHwCal->getAutocal(&actual));
+}
+
+TEST_F(HwCalTest, lra_period_present) {
+    uint32_t expect = std::rand();
+    uint32_t actual = ~expect;
+
+    write("lra_period", expect);
+
+    createHwCal();
+
+    EXPECT_TRUE(mHwCal->getLraPeriod(&actual));
+    EXPECT_EQ(expect, actual);
+}
+
+TEST_F(HwCalTest, lra_period_missing) {
+    uint32_t actual;
+
+    createHwCal();
+
+    EXPECT_FALSE(mHwCal->getLraPeriod(&actual));
+}
+
+TEST_F(HwCalTest, multiple) {
+    std::string autocalExpect = std::to_string(std::rand()) + " " + std::to_string(std::rand()) +
+                                " " + std::to_string(std::rand());
+    std::string autocalActual = "";
+    uint32_t lraPeriodExpect = std::rand();
+    uint32_t lraPeriodActual = ~lraPeriodExpect;
+
+    write("autocal", autocalExpect);
+    write("lra_period", lraPeriodExpect);
+
+    createHwCal();
+
+    EXPECT_TRUE(mHwCal->getAutocal(&autocalActual));
+    EXPECT_EQ(autocalExpect, autocalActual);
+    EXPECT_TRUE(mHwCal->getLraPeriod(&lraPeriodActual));
+    EXPECT_EQ(lraPeriodExpect, lraPeriodActual);
+}
+
+TEST_F(HwCalTest, trimming) {
+    std::string autocalExpect = std::to_string(std::rand()) + " " + std::to_string(std::rand()) +
+                                " " + std::to_string(std::rand());
+    std::string autocalActual = "";
+    uint32_t lraPeriodExpect = std::rand();
+    uint32_t lraPeriodActual = ~lraPeriodExpect;
+
+    write("autocal", autocalExpect, " \t", "\t ");
+    write("lra_period", lraPeriodExpect, " \t", "\t ");
+
+    createHwCal();
+
+    EXPECT_TRUE(mHwCal->getAutocal(&autocalActual));
+    EXPECT_EQ(autocalExpect, autocalActual);
+    EXPECT_TRUE(mHwCal->getLraPeriod(&lraPeriodActual));
+    EXPECT_EQ(lraPeriodExpect, lraPeriodActual);
+}
+
+}  // namespace implementation
+}  // namespace V1_2
+}  // namespace vibrator
+}  // namespace hardware
+}  // namespace android
