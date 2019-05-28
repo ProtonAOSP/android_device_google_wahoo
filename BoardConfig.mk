f0489c2c31f60cb2d54d55007cdc1bd182e42f00
Felix <google@ix5.org>
2019-05-28 14:05:51 +0200

    sepolicy: Use BOARD_VENDOR_SEPOLICY_DIRS
    BOARD_SEPOLICY_DIRS is deprecated.
    
    Change-Id: I046282b2a2e8c541726fb29cb0044503322d4be9

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -87,10 +87,10 @@ BOARD_USES_SYSTEM_OTHER_ODEX := true
 
 BOARD_ROOT_EXTRA_FOLDERS := persist firmware
 
-BOARD_SEPOLICY_DIRS += device/google/wahoo/sepolicy/vendor
+BOARD_VENDOR_SEPOLICY_DIRS += device/google/wahoo/sepolicy/vendor
 BOARD_PLAT_PUBLIC_SEPOLICY_DIR := device/google/wahoo/sepolicy/public
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR := device/google/wahoo/sepolicy/private
-BOARD_SEPOLICY_DIRS += device/google/wahoo/sepolicy/verizon
+BOARD_VENDOR_SEPOLICY_DIRS += device/google/wahoo/sepolicy/verizon
 
 TARGET_FS_CONFIG_GEN := device/google/wahoo/config.fs
 
