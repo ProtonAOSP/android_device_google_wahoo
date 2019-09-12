844b9a2f5e8bc49b03fcfeb8a7c14fe55536d27e
Bowgo Tsai <bowgotsai@google.com>
2019-09-12 13:28:00 +0800

    Revert "Adding GSI keys"
    This reverts commit 421986004d9044f8deed5aa4e90be27dda4d01c8.
    Wahoo has no first-stage ramdisk and there is no need to install
    GSI keys there.
    
    Bug: 140873290
    Test: tree hugger
    
    Change-Id: I4dfe0bb0d88dbbbcaa6742cef9f2405d3b7936d6

--- device.mk
+++ device.mk
@@ -14,9 +14,6 @@
 # limitations under the License.
 #
 
-# Installs gsi keys into ramdisk, to boot a GSI with verified boot.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
-
 # Enable updating of APEXes
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
