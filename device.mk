b2d32df8226ad4db459fa0f8feddc4c7e7c4c306
Paul Scovanner <pscovanner@google.com>
2020-04-13 18:45:53 +0000

    Update Wahoo SVN to 44 Bug: 153882927
    Change-Id: I426d08a4353d7ad1c25954a2b32cb9aae5ffb886

--- device.mk
+++ device.mk
@@ -47,7 +47,7 @@ PRODUCT_COPY_FILES += \
 
 # Set the SVN for the targeted MR release
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.vendor.build.svn=43
+    ro.vendor.build.svn=44
 
 # Enforce privapp-permissions whitelist
 PRODUCT_PROPERTY_OVERRIDES += \
