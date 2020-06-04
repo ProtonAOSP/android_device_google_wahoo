0deece693ccf8018870c1aea4acc585e4ea25b22
Dmitry Dmitriev <dmitriev.dd@hotmail.com>
2020-06-04 22:20:02 +0300

    wahoo: SystemUIGoogle -> SystemUI
    Change-Id: Id9b40eddabdd9a05d0b1128a332d3a8ebb9a3a23

--- device.mk
+++ device.mk
@@ -695,7 +695,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Preopt SystemUI
 PRODUCT_DEXPREOPT_SPEED_APPS += \
-    SystemUIGoogle
+    SystemUI
 
 # audio effects config
 PRODUCT_PROPERTY_OVERRIDES += \
